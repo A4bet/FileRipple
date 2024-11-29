335d301c723bf5da613a998b55cff5e905902b28
A4bet <sblolroblox@gmail.com>
2024-11-28 21:58:33 -0500

    Update routes.go

--- routes.go
+++ routes.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/didip/tollbooth_gin"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type files struct {
@@ -31,12 +36,20 @@ type uriDownload struct {
 }
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.Use(gin.Recovery())
 
+	lmt := tollbooth.NewLimiter(2, nil)
+	lmt = tollbooth.NewLimiter(2, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Hour})
+
+	r.Use(tollbooth_gin.LimitHandler(lmt))
+
 	r.LoadHTMLGlob("HTMLtemplates/*.html")
 	r.Static("/static", "static")
+	r.StaticFile("/favicon.ico", "static/favicon.ico")
+
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(err)
@@ -61,11 +74,13 @@ func main() {
 			c.SetCookie("user", uuid.New().String(), 3600*24*365, "/", "localhost", true, true)
 			c.HTML(200, "history.html", gin.H{})
 		} else {
+			ShaUSR := sha256.New()
+			ShaUSR.Write([]byte(usr))
 			var files []files
-			db.Find(&files, "user = ?", usr)
+			db.Find(&files, "user = ?", hex.EncodeToString(ShaUSR.Sum(nil)))
 			c.HTML(200, "history.html", gin.H{
 				"files": files,
-				"user":  usr,
+				"user":  strings.Split(usr, "-")[1] + strings.Split(usr, "-")[0],
 			})
 		}
 	})
@@ -100,11 +115,12 @@ func main() {
 			}
 			fileName := sanitizeFileName(file.Filename)
 			filePath := fmt.Sprintf("uploadedfiles/%s", fileUUID)
-
+			ShaUSR := sha256.New()
+			ShaUSR.Write([]byte(usr))
 			c.SaveUploadedFile(file, filePath)
-			dbEntry := files{Fname: fileUUID, Frename: fileName, User: usr}
+			dbEntry := files{Fname: fileUUID, Frename: fileName, User: hex.EncodeToString(ShaUSR.Sum(nil))}
 			db.Create(&dbEntry)
-			c.String(200, usr+"/"+file.Filename)
+			c.String(200, hex.EncodeToString(ShaUSR.Sum(nil))+"/"+file.Filename)
 		} else {
 			c.SetCookie("user", uuid.New().String(), 3600*24*365, "/", "localhost", true, true)
 			fileUUID := uuid.New().String()
@@ -120,11 +136,12 @@ func main() {
 			}
 			fileName := sanitizeFileName(file.Filename)
 			filePath := fmt.Sprintf("uploadedfiles/%s", fileUUID)
-
+			ShaUSR := sha256.New()
+			ShaUSR.Write([]byte(usr))
 			c.SaveUploadedFile(file, filePath)
-			dbEntry := files{Fname: fileUUID, Frename: fileName, User: usr}
+			dbEntry := files{Fname: fileUUID, Frename: fileName, User: hex.EncodeToString(ShaUSR.Sum(nil))}
 			db.Create(&dbEntry)
-			c.String(200, usr+"/"+file.Filename)
+			c.String(200, hex.EncodeToString(ShaUSR.Sum(nil))+"/"+file.Filename)
 		}
 	})
 
@@ -139,6 +156,31 @@ func main() {
 		filez := files{}
 		db.First(&filez, "Frename = ? AND User = ?", file, usr)
 		c.File("uploadedfiles/" + sanitizeFileName(filez.Fname))
+	})
+
+	r.GET("/api/v1/delete/:user/:file", func(c *gin.Context) {
+		user, err := c.Cookie("user")
+		if err != nil || user == "" {
+			return
+		}
+		var uriDownload uriDownload
+		if err := c.ShouldBindUri(&uriDownload); err != nil {
+			fmt.Println(err)
+			return
+		}
+		usr := uriDownload.User
+		file := uriDownload.File
+		ShaUSR := sha256.New()
+		ShaUSR.Write([]byte(user))
+		if usr != hex.EncodeToString(ShaUSR.Sum(nil)) {
+			c.String(200, "error: unauthorized")
+			return
+		}
+		filez := files{}
+		db.First(&filez, "Frename = ? AND user = ?", file, usr)
+		db.Delete(&filez)
+		os.Remove("uploadedfiles/" + sanitizeFileName(filez.Fname))
+		c.String(200, "deleted")
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
